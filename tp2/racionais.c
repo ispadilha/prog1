03bcb64145da81023ec730f1775386739f719d41
John Doe <israel.padilha@gmail.com>
2023-08-31 18:45:28 -0300

    Created remaining tp2 functions

--- tp2/racionais.c
+++ tp2/racionais.c
@@ -57,6 +57,32 @@ struct racional cria_r (int numerador, int denominador){
     return r;
 }
 
+int simplifica_r (struct racional *r){
+    /* Retorna 0 caso receba um racional inválido. */
+    if (r->valido == 0) return 0;
+    
+    /* Aproveitei a função criada anteriormente, para o cálculo do MDC.
+    A função simplifica o racional recebido através da divisão de ambos, numerador e denominador, pelo MDC entre os dois. */
+    int mdc_r = mdc(r->num, r->den);
+    r->num = ((r->num) / mdc_r);
+    r->den = ((r->den) / mdc_r);
+
+    /* Se ambos, numerador e denominador, forem negativos, deverá retornar um positivo. */
+    if((r->num < 0) && (r->den < 0)){
+        r->num = r->num * (-1);
+        r->den = r->den * (-1);
+    }
+
+    /* Se apenas o denominador for negativo, o sinal deve migrar para o numerador. */
+    if((r->den < 0)){
+        r->num = r->num * (-1);
+        r->den = r->den * (-1);
+    }
+
+    /* Retorna 1 mediante operação bem sucedida */
+    return 1;
+}
+
 struct racional sorteia_r (int max){
     int den;
     do {
@@ -100,7 +126,7 @@ void imprime_r (struct racional r){
         printf("INVALIDO");
         return;
     } else {
-        simplifica_r(r);
+        simplifica_r(&r);
     }
 
     if ((r.num == 0) || (r.den == 1)){
@@ -117,31 +143,104 @@ void imprime_r (struct racional r){
     return;
 }
 
-int simplifica_r (struct racional *r){
-    if (r->valido == 0) return 0;
-    /* Retorna 0 caso receba um racional inválido.
-    Continua a execução e retornará 1 mediante operação seja bem sucedida */
-    
-    int mdc_r = mdc(r->num, r->den);
-    r->num = ((r->num) / mdc_r);
-    r->den = ((r->den) / mdc_r);
-
-    /* Aproveitei a função criada anteriormente, para o cálculo do MDC.
-    A função simplifica o racional recebido através da divisão de ambos, numerador e denominador, pelo MDC entre os dois. */
-
-    if((r->num < 0) && (r->den < 0)){
-        r->num = r->num * (-1);
-        r->den = r->den * (-1);
+int compara_r(struct racional r1, struct racional r2) {
+    /* Retorna 0 caso receba um racional inválido. */
+    if (!valido_r(r1) || !valido_r(r2)) {
+        return 0;
     }
 
-    /* Se ambos, numerador e denominador, forem negativos, deverá retornar um positivo. */
+    /* Usarei o MMC entre os denominadores, e com ele calcularei os novos numeradores, para poder comparar os dois racionais */
+    int mmc_den = mmc(r1.den, r2.den);
+    int num1 = r1.num * (mmc_den / r1.den);
+    int num2 = r2.num * (mmc_den / r2.den);
 
-    if((r->den < 0)){
-        r->num = r->num * (-1);
-        r->den = r->den * (-1);
+    if (num1 < num2) {
+        return -1;
+    } else if (num1 > num2) {
+        return 1;
+    } else {
+        return 0;
+    }
+    /* Conforme o enunciado: */
+    /* Retorna -1 se r1 < r2; 0 se r1 = r2; 1 se r1 > r2 */
+}
+
+
+int soma_r (struct racional r1, struct racional r2, struct racional *r3) {
+    /* Retorna 0 caso receba um racional inválido. */
+    if (!valido_r(r1) || !valido_r(r2)){
+        return 0;
+    }
+   
+    int d = mmc(r1.den, r2.den);
+    int n = ((d/r1.den) * r1.num) + ((d/r2.den) * r2.num);
+
+    /* Uso "d" e "n" como variáveis para numerador e denominador, no escopo local;
+    O denominador é o MMC entre as frações (números racionais), conforme a operação matemática correspondente */
+
+    r3->num = n;
+    r3->den = d;
+
+    simplifica_r(r3);
+
+    r3->valido = valido_r(*r3);
+
+    /* Retorna 1 mediante operação bem sucedida */
+    return 1;
+}
+
+int subtrai_r (struct racional r1, struct racional r2, struct racional *r3) {
+    /* Retorna 0 caso receba um racional inválido. */
+    if (!valido_r(r1) || !valido_r(r2)){
+        return 0;
+    }
+   
+    int d = mmc(r1.den, r2.den);
+    int n = ((d/r1.den) * r1.num) - ((d/r2.den) * r2.num);
+
+    /* Bastante semelhante à função de soma, tomando o cuidado devido com o sinal */
+
+    r3->num = n;
+    r3->den = d;
+
+    simplifica_r(r3);
+
+    r3->valido = valido_r(*r3);
+
+    /* Retorna 1 mediante operação bem sucedida */
+    return 1;
+}
+
+int multiplica_r (struct racional r1, struct racional r2, struct racional *r3){
+    /* Retorna 0 caso receba um racional inválido. */
+    if (!valido_r(r1) || !valido_r(r2)){
+        return 0;
     }
 
-    /* Se apenas o denominador for negativo, o sinal deve migrar para o numerador. */
+    r3->num = (r1.num * r2.num);
+    r3->den = (r1.den * r2.den);
 
+    simplifica_r(r3);
+
+    r3->valido = valido_r(*r3);
+
+    /* Retorna 1 mediante operação bem sucedida */
+    return 1;
+}
+
+int divide_r (struct racional r1, struct racional r2, struct racional *r3){
+    /* Retorna 0 caso receba um racional inválido. */
+    if (!valido_r(r1) || !valido_r(r2)){
+        return 0;
+    }
+
+    r3->num = (r1.num * r2.den);
+    r3->den = (r1.den * r2.num);
+
+    simplifica_r(r3);
+
+    r3->valido = valido_r(*r3);
+
+    /* Retorna 1 mediante operação bem sucedida */
     return 1;
 }
