c5570d53721ab53ad06d464e8f8b375a2beee016
John Doe <israel.padilha@gmail.com>
2023-08-31 17:05:07 -0300

    Imported tp2 initial files and question statement

---
+++ tp2/racionais.c
@@ -0,0 +1,28 @@
+#include "racionais.h"
+/* acrescente demais includes que voce queira ou precise */
+
+/* 
+ * Implemente aqui as funcoes definidas no racionais.h.
+ * Caso queira, você pode definir neste arquivo funcoes
+ * adicionais que serao internas a este arquivo.
+ *
+ * Por exemplo, as funções aleat, mdc e mmc devem ser
+ * implementadas neste arquivo.
+*/
+
+/* retorna um numero aleatorio entre min e max, inclusive. */
+int aleat (int min, int max){
+/* coloque o codigo aqui */
+}
+
+/* Maximo Divisor Comum entre a e b      */
+/* calcula o mdc pelo metodo de Euclides */
+int mdc (int a, int b){
+/* coloque o codigo aqui */
+}
+
+/* Minimo Multiplo Comum entre a e b */
+/* mmc = (a * b) / mdc (a, b)        */
+int mmc (int a, int b){
+/* coloque o codigo aqui */
+}
