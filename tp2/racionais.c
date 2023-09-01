f20cc67d5f43734e6d946b146705aa1f3d6586d1
John Doe <israel.padilha@gmail.com>
2023-09-01 18:36:19 -0300

    Greatly improved and polished codes

--- tp2/racionais.c
+++ tp2/racionais.c
@@ -2,23 +2,16 @@
 #include <stdlib.h>
 #include "racionais.h"
 
-/* retorna um numero aleatorio entre min e max, inclusive. */
+/* Retorna um número aleatório entre -max e max: */
 int aleat (int min, int max){
-    int tamanho = max - min;
-    int x = (rand() % (tamanho + 1));
-    x = x + min;
+    int tamanho = 2*max;
+    int aux = (rand() % (tamanho + 1));
+    int resultado = aux - max + min;
 
-    /* A operação módulo retorna um resto de divisão,
-    que estará dentro do intervalo correspondente a (tamanho - 1).
-    Para corrigir isto, calculo o módulo por (tamanho + 1).
-    Isto sorteia um número entre 0 e o tamanho. Por isso, somo (x + min).
-    Assim, o sorteio será entre min e max. */
-
-    return x;
+    return resultado;
 }
 
-/* Maximo Divisor Comum entre a e b      */
-/* calcula o mdc pelo metodo de Euclides */
+/* Calcula o mdc pelo método de Euclides: */
 int mdc (int a, int b){
     while (b != 0) {
         int resto = a % b;
@@ -26,19 +19,12 @@ int mdc (int a, int b){
         b = resto;
     }
 
-    /* O algoritmo de Euclides mantém um laço de repetição enquanto "b" não for zero.
-    A cada iteração, atualiza "a" com o valor que estava em "b", e atualiza "b" com o resto da divisão.
-    Quando o resto for zero, a repetição termina. E o resultado, (ou seja, o MDC) estará armazenado em "a". */
-
     return a;
 }
 
-/* Minimo Multiplo Comum entre a e b */
-/* mmc = (a * b) / mdc (a, b)        */
+/* Mínimo Multiplo Comum entre a e b, aproveitando a função MDC acima: */
 int mmc (int a, int b){
     int x = (a*b)/mdc(a,b);
-
-    /* Aproveitando a função MDC. */
 
     return x;
 }
@@ -51,70 +37,62 @@ struct racional cria_r (int numerador, int denominador){
     if(denominador != 0){
         r.valido = 1;
     } else {
+        /* Se o valor do denominador for igual a zero, o campo "valido" da struct também recebe zero, assinalando como inválido: */
         r.valido = 0;
     }
-
-    /* Se o valor do denominador for igual a zero, o campo "valido" da struct também recebe zero, assinalando como inválido. */
 
     return r;
 }
 
+/* Recebe um número racional no parâmetro e o simplifica: */
 int simplifica_r (struct racional *r){
-    /* Retorna 0 caso receba um racional inválido. */
+    /* Retorna 0 caso receba um racional inválido: */
     if (r->valido == 0) return 0;
     
-    /* Aproveitei a função criada anteriormente, para o cálculo do MDC.
-    A função simplifica o racional recebido através da divisão de ambos, numerador e denominador, pelo MDC entre os dois. */
+    /* Simplifica o racional através da divisão de numerador e denominador pelo MDC entre os dois: */
     int mdc_r = mdc(r->num, r->den);
     r->num = ((r->num) / mdc_r);
     r->den = ((r->den) / mdc_r);
 
-    /* Se ambos, numerador e denominador, forem negativos, deverá retornar um positivo. */
+    /* Se numerador e denominador forem negativos, deverá resultar em positivo: */
     if((r->num < 0) && (r->den < 0)){
         r->num = r->num * (-1);
         r->den = r->den * (-1);
     }
 
-    /* Se apenas o denominador for negativo, o sinal deve migrar para o numerador. */
+    /* Se apenas o denominador for negativo, o sinal deve migrar para o numerador: */
     if((r->den < 0)){
         r->num = r->num * (-1);
         r->den = r->den * (-1);
     }
 
-    /* Retorna 1 mediante operação bem sucedida */
+    /* Retorna 1 mediante operação bem sucedida: */
     return 1;
 }
 
+/* Retorna um número racional aleatório na forma simplificada: */
 struct racional sorteia_r (int max){
-    int den;
-    do {
-        den = (rand() % max);
-    } while ( den == 0 ) ;
-
-    /* Um laço de repetição assegura que o valor do denominador seja diferente de zero. */
-
-    int num = (rand() % max);
+    int den = (aleat(0, max));
+    int num = (aleat(0, max));
     struct racional r = cria_r(num, den);
-
-    /* Usei variáveis locais de inteiros para numerador e para denominador,
-    para poder usar a função anterior, de criação de um número racional.  */
+    simplifica_r(&r);
 
     return r;
 }
 
-/* Retorna o numerador de um racional */
+/* Retorna o numerador de um racional: */
 int numerador_r (struct racional r){
     return r.num;
 }
 
-/* Retorna o denominador de um racional */
+/* Retorna o denominador de um racional: */
 int denominador_r (struct racional r){
     return r.den;
 }
 
+/* Retorna 1 se o racional r é válido ou 0 caso contrário;
+Um racional é inválido se o denominador for nulo: */
 int valido_r (struct racional r){
-
-    /* Um racional eh invalido se o denominador for nulo */
     
     if (r.den == 0){
         return 0;
@@ -125,7 +103,7 @@ int valido_r (struct racional r){
 
 void imprime_r (struct racional r){
     if (!valido_r(r)){
-        printf("INVALIDO");
+        printf("INVALIDO ");
         return;
     } else {
         simplifica_r(&r);
@@ -151,11 +129,13 @@ int compara_r(struct racional r1, struct racional r2) {
         return 0;
     }
 
-    /* Usarei o MMC entre os denominadores, e com ele calcularei os novos numeradores, para poder comparar os dois racionais */
+    /* Usando o MMC entre os denominadores, e com ele calculando os novos numeradores, para poder comparar os dois racionais */
     int mmc_den = mmc(r1.den, r2.den);
     int num1 = r1.num * (mmc_den / r1.den);
     int num2 = r2.num * (mmc_den / r2.den);
 
+    /* Conforme o enunciado:
+    Retorna -1 se r1 < r2; 0 se r1 = r2; 1 se r1 > r2 */
     if (num1 < num2) {
         return -1;
     } else if (num1 > num2) {
@@ -163,8 +143,6 @@ int compara_r(struct racional r1, struct racional r2) {
     } else {
         return 0;
     }
-    /* Conforme o enunciado: */
-    /* Retorna -1 se r1 < r2; 0 se r1 = r2; 1 se r1 > r2 */
 }
 
 
@@ -173,21 +151,19 @@ int soma_r (struct racional r1, struct racional r2, struct racional *r3) {
     if (!valido_r(r1) || !valido_r(r2)){
         return 0;
     }
-   
-    int d = mmc(r1.den, r2.den);
-    int n = ((d/r1.den) * r1.num) + ((d/r2.den) * r2.num);
 
     /* Uso "d" e "n" como variáveis para numerador e denominador, no escopo local;
     O denominador é o MMC entre as frações (números racionais), conforme a operação matemática correspondente */
+    int d = mmc(r1.den, r2.den);
+    int n = ((d/r1.den) * r1.num) + ((d/r2.den) * r2.num);
 
     r3->num = n;
     r3->den = d;
 
-    simplifica_r(r3);
+    /* Soma entre racionais válidos sempre será válida: */
+    r3->valido = 1;
 
-    r3->valido = valido_r(*r3);
-
-    /* Retorna 1 mediante operação bem sucedida */
+    /* Retorna 1 mediante operação bem sucedida: */
     return 1;
 }
 
@@ -197,19 +173,17 @@ int subtrai_r (struct racional r1, struct racional r2, struct racional *r3) {
         return 0;
     }
    
+    /* Bastante semelhante à função de soma, tomando o cuidado devido com o sinal */
     int d = mmc(r1.den, r2.den);
     int n = ((d/r1.den) * r1.num) - ((d/r2.den) * r2.num);
-
-    /* Bastante semelhante à função de soma, tomando o cuidado devido com o sinal */
-
+    
     r3->num = n;
     r3->den = d;
 
-    simplifica_r(r3);
+    /* Subtração entre racionais válidos sempre será válida: */
+    r3->valido = 1;
 
-    r3->valido = valido_r(*r3);
-
-    /* Retorna 1 mediante operação bem sucedida */
+    /* Retorna 1 mediante operação bem sucedida: */
     return 1;
 }
 
@@ -222,9 +196,8 @@ int multiplica_r (struct racional r1, struct racional r2, struct racional *r3){
     r3->num = (r1.num * r2.num);
     r3->den = (r1.den * r2.den);
 
-    simplifica_r(r3);
-
-    r3->valido = valido_r(*r3);
+    /* Multiplicação entre racionais válidos sempre será válida: */
+    r3->valido = 1;
 
     /* Retorna 1 mediante operação bem sucedida */
     return 1;
@@ -239,8 +212,7 @@ int divide_r (struct racional r1, struct racional r2, struct racional *r3){
     r3->num = (r1.num * r2.den);
     r3->den = (r1.den * r2.num);
 
-    simplifica_r(r3);
-
+    /* Testa se o racional resultante também é válido: */
     r3->valido = valido_r(*r3);
 
     /* Retorna 1 mediante operação bem sucedida */
--- tp2/tp2.c
+++ tp2/tp2.c
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include "racionais.h"
 
+/* Tamanho máximo do vetor: */
+#define TMAX 100
+
+/* Valor máximo de numerador e denominador para os números racionais utilizados: */
+#define VMAX 10
+
 int main() {
      /* Inicializando a semente randômica: */
     srand(0);
@@ -11,11 +17,13 @@ int main() {
     /* Declarando variáveis que serão usadas como índices em laços de repetição: */
     int i = 0, j = 0, k = 0;
 
+    /* e a variável do tipo inteiro que receberá do usuário o tamanho do vetor de racionais: */
     int n;
-    printf("\nDigite um valor para n (0 < n < 100): ");
+    
+    printf("Digite um valor para n (0 < n < %d): ", TMAX);
     scanf("%d", &n);
 
-    if (n <= 0 || n >= 100) {
+    if (n <= 0 || n >= TMAX) {
         printf("\nO valor inserido está fora do intervalo permitido!\n");
         return 1;
     }
@@ -24,58 +32,71 @@ int main() {
     struct racional vetor[n];
 
     for (i = 0; i < n; i++) {
-        vetor[i] = sorteia_r(100);
+        vetor[i] = sorteia_r(VMAX);
     }
 
     for (i = 0; i < n; i++) {
         imprime_r(vetor[i]);
     }
 
-    /* Filtrando racionais inválidos, usando a segunda variável de índice: */
+    /* Quebras de linha após impressões do vetor, conforme enunciado: */
+    printf("\n");
+
+    /* Filtrando racionais inválidos, usando a segunda variável de índice;
+    E usando um novo vetor para não manipular o vetor original, aqui de dentro da função main,
+    evitando assim uma violação de Tipo Abstrato de Dados: */
+
+    struct racional vetor_validos[TMAX];
     for (i = 0; i < n; i++) {
         if (valido_r(vetor[i])) {
-            vetor[j] = vetor[i];
+            vetor_validos[j] = vetor[i];
             j++;
         }
     }
-
-    /* O novo vetor poderá ser menor, devido a eliminações de elementos inválidos; por isso o laço irá até o "j" encontrado acima: */
+   
     for (i = 0; i < j; i++) {
-        imprime_r(vetor[i]);
+        imprime_r(vetor_validos[i]);
     }
+
+    printf("\n");
 
     /* Ordenando o vetor, utilizando o algoritmo Selection Sort: */
     for (i = 0; i < j - 1; i++) {
         int i_menor = i;
 
         for (k = i + 1; k < j; k++) {
-            if (compara_r(vetor[k], vetor[i_menor]) < 0) {
+            if (compara_r(vetor_validos[k], vetor_validos[i_menor]) < 0) {
                 i_menor = k;
             }
         }
 
         if (i_menor != i) {
-            struct racional temp = vetor[i];
-            vetor[i] = vetor[i_menor];
-            vetor[i_menor] = temp;
+            struct racional temp = vetor_validos[i];
+            vetor_validos[i] = vetor_validos[i_menor];
+            vetor_validos[i_menor] = temp;
         }
     }
-    /* O algoritmo Selection Sort garante que, a cada iteração, o menor elemento do vetor estará em sua posição definitiva. */
 
     for (i = 0; i < j; i++) {
-        imprime_r(vetor[i]);
+        imprime_r(vetor_validos[i]);
     }
 
-    // Calculando e imprimindo a soma dos elementos do vetor
+    printf("\n");
+
+    /* Para calcular a soma de todos os elementos do vetor:
+    Inicialmente, declaramos um racional inicializado em 0;
+    ou seja, 0/1: */
     struct racional soma = cria_r(0, 1);
+    struct racional temp = cria_r(0, 1);
+
     for (i = 0; i < j; i++) {
-        struct racional temp;
-        if (soma_r(soma, vetor[i], &temp)) {
+        if (soma_r(soma, vetor_validos[i], &temp)) {
             soma = temp;
+            simplifica_r(&soma);
         }
     }
 
-    printf("\nSoma de todos os elementos: ");
+    printf("Soma de todos os elementos: ");
     imprime_r(soma);
     printf("\n");
 
