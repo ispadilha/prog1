121039345af7c0b3748dd3874bf27281d2af43cc
John Doe <israel.padilha@gmail.com>
2023-09-14 17:19:00 -0300

    Created first functions

--- tp3/racionais.c
+++ tp3/racionais.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "racionais.h"
 
 /* A diferenca desta implementação com relacao ao TP2
@@ -17,13 +19,92 @@
 /* Maximo Divisor Comum entre a e b      */
 /* calcula o mdc pelo metodo de Euclides */
 int mdc (int a, int b){
-/* coloque o codigo aqui */
+    while (b != 0) {
+        int resto = a % b;
+        a = b;
+        b = resto;
+    }
+
+    return a;
 }
 
 /* Minimo Multiplo Comum entre a e b */
 /* mmc = (a * b) / mdc (a, b)        */
 int mmc (int a, int b){
-/* coloque o codigo aqui */
+    int x = (a*b)/mdc(a,b);
+
+    return x;
 }
 
 /* Implemente aqui as funcoes cujos prototipos estao em racionais.h */
+
+struct racional *cria_r (long int numerador, long int denominador){
+    struct racional *r;
+
+    r = malloc(sizeof(struct racional *));
+    if (!r) 
+        return NULL;
+
+    r->num = numerador;
+    r->den = denominador;
+
+    if (valido_r(r)){
+        simplifica_r(r);
+    }
+    return r;
+}
+
+int valido_r (struct racional *r){
+    if (r->den == 0){
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+void imprime_r (struct racional *r){
+    if (!valido_r(r)){
+        printf("INVALIDO ");
+        return;
+    } else {
+        simplifica_r(r);
+    }
+
+    if ((r->num == 0) || (r->den == 1)){
+        printf("%ld ", r->num);
+        return;
+    }
+
+    if (r->num == r->den) {
+        printf("1 ");
+        return;
+    }
+
+    printf("%ld/%ld ", r->num, r->den);
+    return;
+}
+
+int simplifica_r (struct racional *r){
+        /* Retorna 0 caso receba um racional inválido: */
+    if (valido_r(r) == 0) return 0;
+    
+    /* Simplifica o racional através da divisão de numerador e denominador pelo MDC entre os dois: */
+    int mdc_r = mdc(r->num, r->den);
+    r->num = ((r->num) / mdc_r);
+    r->den = ((r->den) / mdc_r);
+
+    /* Se numerador e denominador forem negativos, deverá resultar em positivo: */
+    if((r->num < 0) && (r->den < 0)){
+        r->num = r->num * (-1);
+        r->den = r->den * (-1);
+    }
+
+    /* Se apenas o denominador for negativo, o sinal deve migrar para o numerador: */
+    if((r->den < 0)){
+        r->num = r->num * (-1);
+        r->den = r->den * (-1);
+    }
+
+    /* Retorna 1 mediante operação bem sucedida: */
+    return 1;
+}
--- tp3/tp3.c
+++ tp3/tp3.c
@@ -5,11 +5,40 @@
 /* coloque funcoes desejadas aqui. 
  * Exemplo: ordenar, eliminar invalidos... */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "racionais.h"
+
+#define MAX 100
+
 int main (){
     /* vetor de ponteiros para racionais */
     struct racional **v;  /* equivalente a struct racional *v[] */
 
-    /* coloque seu codigo aqui */
+    int i, n;
+    long int a, b;
+
+    do(
+        scanf("%d", &n)
+    );
+    while (
+        n < 1 || n > MAX
+    );
+    
+    v = malloc (sizeof(struct racional*)*n);
+    
+    for (i = 0; i<n; i++){
+        /* usar a funcao cria_r */
+        scanf("%ld %ld", &a, &b);
+        v[i] = cria_r(a, b);
+    }
+
+    for (i=0; i<n; i++){
+        imprime_r(v[i]);
+    }
+    
+    printf("\n");
 
     return 0;
+
 }
