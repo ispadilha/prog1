a795bcd8a6d4bc8e35e401828ab65b8536328c87
John Doe <israel.padilha@gmail.com>
2023-09-14 15:55:15 -0300

    Imported tp3 initial files

---
+++ tp3/racionais.c
@@ -0,0 +1,29 @@
+#include "racionais.h"
+
+/* A diferenca desta implementação com relacao ao TP2
+ * eh que os racionais devem alocados dinamicamente */
+
+/* acrescente demais includes que voce queira ou precise */
+
+/* 
+ * Implemente aqui as funcoes definidas no racionais.h.
+ * Caso queira, voce pode definir neste arquivo funcoes
+ * adicionais que serao internas a este arquivo.
+ *
+ * Por exemplo, as funções mdc e mmc devem ser
+ * implementadas neste arquivo.
+*/
+
+/* Maximo Divisor Comum entre a e b      */
+/* calcula o mdc pelo metodo de Euclides */
+int mdc (int a, int b){
+/* coloque o codigo aqui */
+}
+
+/* Minimo Multiplo Comum entre a e b */
+/* mmc = (a * b) / mdc (a, b)        */
+int mmc (int a, int b){
+/* coloque o codigo aqui */
+}
+
+/* Implemente aqui as funcoes cujos prototipos estao em racionais.h */
