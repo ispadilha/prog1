afcb2495a9b4cf5186070267065c19e6831994dc
ispadilha <israel.padilha@gmail.com>
2024-04-02 19:04:07 -0300

    Implemented cria_r function

--- tp2/racionais.c
+++ tp2/racionais.c
@@ -28,7 +28,7 @@ int mmc (int a, int b){
 
 void simplifica_r(struct racional *r) {
     /* A função simplifica o racional recebido através da divisão de ambos, numerador e denominador, pelo MDC entre os dois;
-    com sintaxe adaptada para o uso de ponteiros */
+    com sintaxe adaptada para o uso de ponteiros: */
     int mdc_r = mdc(r->num, r->den);
     r->num = (r->num / mdc_r);
     r->den = (r->den / mdc_r);
@@ -47,6 +47,19 @@ void simplifica_r(struct racional *r) {
 
 /* aqui voce pode definir mais funcoes internas, caso queira ou precise */
 
-/*
- * Implemente aqui as funcoes definidas no racionais.h.
-*/
+struct racional cria_r (int numerador, int denominador){
+    struct racional r;
+    r.num = numerador;
+    r.den = denominador;
+
+    /* struct racional consiste nos campos numerador, denominador, e "valido", portanto isso faz parte da criação do racional: */
+    if(r.den == 0)
+        r.valido = 0;
+    else
+        r.valido = 1;
+
+    /* Simplifica o racional antes de retorná-lo, conforme especificação para esta nova versão do programa: */
+    simplifica_r(&r);
+
+    return r;
+}
