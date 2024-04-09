f86a38b1a760fe21c515c0fe4d689fa384396e1d
John Doe <israel.padilha@gmail.com>
2024-04-09 13:49:05 -0300

    Reset tp3

--- tp3/racionais.c
+++ tp3/racionais.c
@@ -1,219 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "racionais.h"
+/* coloque demais includes aqui */
 
-/* A diferenca desta implementação com relacao ao TP2
- * eh que os racionais devem alocados dinamicamente */
+/* Funcoes privadas restritas a este modulo.
+ * Voce deve implementa-las pois serao necessarias,
+ * embora elas nao precisem estar no .h */
 
-/* acrescente demais includes que voce queira ou precise */
+/* retorna um numero aleatorio entre min e max, inclusive. */
+int aleat (long int min, long int max){
+    /* coloque o codigo aqui */
+}
 
-/* 
- * Implemente aqui as funcoes definidas no racionais.h.
- * Caso queira, voce pode definir neste arquivo funcoes
- * adicionais que serao internas a este arquivo.
- *
- * Por exemplo, as funções mdc e mmc devem ser
- * implementadas neste arquivo.
-*/
-
-/* Maximo Divisor Comum entre a e b      */
-/* calcula o mdc pelo metodo de Euclides */
-int mdc (int a, int b){
-    while (b != 0) {
-        int resto = a % b;
-        a = b;
-        b = resto;
-    }
-
-    return a;
+/* Maximo Divisor Comum entre a e b.         */
+/* Calcula o mdc pelo metodo de Euclides.    */
+/* Dica: a versao recursiva eh bem elegante! */
+long int mdc (long int a, long int b){
+    /* coloque o codigo aqui */
 }
 
 /* Minimo Multiplo Comum entre a e b */
 /* mmc = (a * b) / mdc (a, b)        */
-int mmc (int a, int b){
-    int x = (a*b)/mdc(a,b);
-
-    return x;
+long int mmc (long int a, long int b){
+    /* coloque o codigo aqui */
 }
+
+/* Recebe um numero racional e o retorna simplificado no parametro *r.
+   Quem chama esta funcao deve garantir que *r seja valido e nao NULL. */
+void simplifica_r (struct racional *r){
+    /* coloque o codigo aqui */
+}
+
+/* aqui voce pode definir mais funcoes internas, caso queira ou precise */
 
 /* Implemente aqui as funcoes cujos prototipos estao em racionais.h */
-
-struct racional *cria_r (long int numerador, long int denominador){
-    struct racional *r;
-
-    r = malloc(sizeof(struct racional));
-    if (!r) {
-        printf("Erro na alocação de memória para o racional.\n");
-        return NULL;
-    }
-
-
-    r->num = numerador;
-    r->den = denominador;
-
-    if (valido_r(r)){
-        simplifica_r(r);
-    }
-    return r;
-}
-
-void destroi_r (struct racional *r){
-    free(r);
-}
-
-int numerador_r (struct racional *r, long int *num){
-    if(!valido_r(r) || r == NULL){
-        return 0;
-    } else {
-        *num = r->num;
-        return 1;
-    }
-}
-
-int denominador_r (struct racional *r, long int *den){
-    if(!valido_r(r) || r == NULL){
-        return 0;
-    } else {
-        *den = r->den;
-        return 1;
-    }
-}
-
-int valido_r (struct racional *r){
-    if (r->den == 0){
-        return 0;
-    } else {
-        return 1;
-    }
-}
-
-void imprime_r (struct racional *r){
-    if (!valido_r(r)){
-        printf("INVALIDO ");
-        return;
-    } else {
-        simplifica_r(r);
-    }
-
-    if ((r->num == 0) || (r->den == 1)){
-        printf("%ld ", r->num);
-        return;
-    }
-
-    if (r->num == r->den) {
-        printf("1 ");
-        return;
-    }
-
-    printf("%ld/%ld ", r->num, r->den);
-    return;
-}
-
-int compara_r (struct racional *r1, struct racional *r2){
-    /* Retorna 0 caso receba um racional inválido. */
-    if (!valido_r(r1) || !valido_r(r2)) {
-        return -2;
-    }
-
-    /* Usando o MMC entre os denominadores, e com ele calculando os novos numeradores, para poder comparar os dois racionais */
-    int mmc_den = mmc(r1->den, r2->den);
-    int num1 = r1->num * (mmc_den / r1->den);
-    int num2 = r2->num * (mmc_den / r2->den);
-
-    /* Conforme o enunciado:
-    Retorna -1 se r1 < r2; 0 se r1 = r2; 1 se r1 > r2 */
-    if (num1 < num2) {
-        return -1;
-    } else if (num1 > num2) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-int simplifica_r (struct racional *r){
-        /* Retorna 0 caso receba um racional inválido: */
-    if (valido_r(r) == 0) return 0;
-    
-    /* Simplifica o racional através da divisão de numerador e denominador pelo MDC entre os dois: */
-    int mdc_r = mdc(r->num, r->den);
-    r->num = ((r->num) / mdc_r);
-    r->den = ((r->den) / mdc_r);
-
-    /* Se numerador e denominador forem negativos, deverá resultar em positivo: */
-    if((r->num < 0) && (r->den < 0)){
-        r->num = r->num * (-1);
-        r->den = r->den * (-1);
-    }
-
-    /* Se apenas o denominador for negativo, o sinal deve migrar para o numerador: */
-    if((r->den < 0)){
-        r->num = r->num * (-1);
-        r->den = r->den * (-1);
-    }
-
-    /* Retorna 1 mediante operação bem sucedida: */
-    return 1;
-}
-
-int soma_r (struct racional *r1, struct racional *r2, struct racional *r3){
-    /* Retorna 0 caso receba um racional inválido ou nulo. */
-    if (!valido_r(r1) || !valido_r(r2) || r1->num == 0 || r2->num == 0){
-        return 0;
-    }
-
-    int d = mmc(r1->den, r2->den);
-    int n = ((d/r1->den) * r1->num) + ((d/r2->den) * r2->num);
-
-    r3->num = n;
-    r3->den = d;
-
-    simplifica_r(r3);
-
-    /* Retorna 1 mediante operação bem sucedida: */
-    return 1;
-}
-
-int subtrai_r (struct racional *r1, struct racional *r2, struct racional *r3){
-    /* Retorna 0 caso receba um racional inválido ou nulo. */
-    if (!valido_r(r1) || !valido_r(r2) || r1->num == 0 || r2->num == 0){
-        return 0;
-    }
-
-    int d = mmc(r1->den, r2->den);
-    int n = ((d/r1->den) * r1->num) - ((d/r2->den) * r2->num);
-
-    r3->num = n;
-    r3->den = d;
-
-    simplifica_r(r3);
-
-    /* Retorna 1 mediante operação bem sucedida: */
-    return 1;
-}
-
-int multiplica_r (struct racional *r1, struct racional *r2, struct racional *r3){
-    /* Retorna 0 caso receba um racional inválido ou nulo. */
-    if (!valido_r(r1) || !valido_r(r2) || r1->num == 0 || r2->num == 0){
-        return 0;
-    }
-
-    r3->num = (r1->num * r2->num);
-    r3->den = (r1->den * r2->den);
-
-    /* Retorna 1 mediante operação bem sucedida */
-    return 1;
-}
-
-int divide_r (struct racional *r1, struct racional *r2, struct racional *r3){
-    /* Retorna 0 caso receba um racional inválido ou nulo. */
-    if (!valido_r(r1) || !valido_r(r2) || r1->num == 0 || r2->num == 0){
-        return 0;
-    }
-
-    r3->num = (r1->num * r2->den);
-    r3->den = (r1->den * r2->num);
-
-    /* Retorna 1 mediante operação bem sucedida */
-    return 1;
-}
--- tp3/tp3.c
+++ tp3/tp3.c
@@ -5,104 +5,11 @@
 /* coloque funcoes desejadas aqui. 
  * Exemplo: ordenar, eliminar invalidos... */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "racionais.h"
+int main (){
+    /* vetor de ponteiros para racionais */
+    struct racional **v;  /* equivalente a struct racional *v[] */
 
-#define MAX 100
-
-int le_n_valido(){
-    int n;
-
-    do {
-        scanf("%d", &n);
-    } while (n < 1 || n > MAX);
-
-    return n;
-}
-
-void imprime_vetor(int i, int n, struct racional **v){
-    for (i = 0; i < n; i++) {
-        imprime_r(v[i]);
-    }
-
-    printf("\n");
-}
-
-void imprime_soma(int i, int j, struct racional **v_validos){
-    /* Para calcular a soma de todos os elementos do vetor:
-    Inicialmente, declaramos um racional inicializado em 0;
-    ou seja, 0/1: */
-    struct racional *soma = cria_r(0, 1);
-    struct racional *temp = cria_r(0, 1);
-
-    for (i = 0; i < j; i++) {
-        if (soma_r(soma, v_validos[i], temp)) {
-            soma = temp;
-            simplifica_r(soma);
-        }
-    }
-
-    printf("Soma de todos os elementos: ");
-    imprime_r(soma);
-    printf("\n");
-}
-
-int main() {
-    struct racional **v;
-    struct racional **v_validos;
-    int i, j, n;
-    long int a, b;
-
-    /* Exige um n valido entre 1 e MAX */
-    n = le_n_valido();
-
-    v = (struct racional **)malloc(sizeof(struct racional *) * n);
-    if (!v) {
-        printf("Erro na alocação de memória.\n");
-        return 1;
-    }
-
-    v_validos = ((struct racional **)malloc(sizeof(struct racional *) * n));
-    if (!v_validos) {
-        printf("Erro na alocação de memória.\n");
-        return 1;
-    }
-
-    for (i = 0; i < n; i++) {
-        scanf("%ld %ld", &a, &b);
-        v[i] = cria_r(a, b);
-        if (!v[i]) {
-            printf("Erro na alocação de memória para o racional %d.\n", i);
-            return 1;
-        }
-    }
-
-    /* Imprime cada racional seguido de um espaco, e pula uma linha apos imprimir o vetor inteiro */
-    imprime_vetor(i, n, v);
-
-    /* Filtrando racionais inválidos, usando a segunda variável de índice e um novo vetor */
-    for (i = 0; i < n; i++) {
-        if (valido_r(v[i])) {
-            v_validos[j] = v[i];
-            j++;
-        }
-    }
-
-    imprime_vetor(i, n, v_validos);
-
-    //Ordenar vetor
-
-    //Calcular e imprimir a soma de todos os racionais do vetor
-    imprime_soma(i,j, v_validos);
-
-    // Liberando a memória alocada para os racionais
-    for (i = 0; i < n; i++) {
-        destroi_r(v[i]);
-    }
-
-    // Liberando o vetor de ponteiros
-    free(v);
+    /* coloque seu codigo aqui */
 
     return 0;
 }
