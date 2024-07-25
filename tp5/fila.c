e239a62aec307a63a1c299abaefc926999344ff8
ispadilha <israel.padilha@gmail.com>
2024-07-24 23:34:49 -0300

    Advanced in fila.c

--- tp5/fila.c
+++ tp5/fila.c
@@ -4,48 +4,79 @@
 
 /* Cria e retorna uma nova fila */
 struct fila *fila_cria () {
-    struct fila *f = (struct fila *) malloc(sizeof(struct fila));
-    if (f) {
-        f->ini = NULL;
-        f->fim = NULL;
-        f->tamanho = 0;
+    struct fila *novaFila = (struct fila *) malloc(sizeof(struct fila));
+    if (novaFila == NULL) {
+        /* Retorna NULL em caso de erro de alocação, conforme especificação */
+        return NULL;
     }
-    return f;
+    novaFila->ini = NULL;
+    novaFila->fim = NULL;
+    novaFila->tamanho = 0;
+    return novaFila;
 }
 
 /* Desaloca toda memoria da fila e faz fila receber NULL */
-void fila_destroi (struct fila **f) {
-    struct nodo *current = (*f)->ini;
-    struct nodo *next;
-    while (current != NULL) {
-        next = current->prox;
-        free(current);
-        current = next;
+void fila_destroi (struct fila **filaADestruir) {
+    struct nodo *atual = (*filaADestruir)->ini;
+    struct nodo *prox;
+    /* Enquanto não encontrar um ponteiro para nodo que seja NULL,
+    a lista não acabou */
+    while (atual != NULL) {
+        prox = atual->prox;
+        free(atual);
+        atual = prox;
     }
-    free(*f);
-    *f = NULL;
+    free(*filaADestruir);
+    *filaADestruir = NULL;
 }
 
 /* Insere dado na fila (politica FIFO). Retorna 1 em caso de sucesso e 0 em caso de falha */
 int enqueue (struct fila *f, int dado) {
     struct nodo *n = (struct nodo *) malloc(sizeof(struct nodo));
-    if (!n) return 0;
+    
+    if (n == NULL){
+        return 0;
+    }
+    
     n->chave = dado;
     n->prox = NULL;
-    if (f->fim) f->fim->prox = n;
+    
+    /* Se já existem elementos, o atual último nodo é ajustado,
+    para apontar para o novo último nodo (que é "n"). */
+    if (f->fim != NULL){
+        f->fim->prox = n;
+    }
+
     f->fim = n;
-    if (!f->ini) f->ini = n;
+
+    if (f->ini == NULL){
+        f->ini = n;
+    }
+
     f->tamanho++;
     return 1;
 }
 
-/* Remove dado da fila (politica FIFO) e retorna o elemento no parametro dado. Retorna 1 em caso de sucesso e 0 em caso de fila vazia */
+/* Remove dado da fila (politica FIFO) e retorna o elemento no parametro dado.
+Retorna 1 em caso de sucesso e 0 em caso de fila vazia */
 int dequeue (struct fila *f, int *dado) {
-    if (f->tamanho == 0) return 0;
+    if (f->tamanho == 0){
+        return 0;
+    }
+    
     struct nodo *n = f->ini;
+
+    /* Retorno no parametro, nao confundindo com retorno da funcao */
     *dado = n->chave;
+
+    /* Atualizando o ponteiro para nodo "ini" (por ser politica FIFO) */
     f->ini = n->prox;
-    if (!f->ini) f->fim = NULL;
+
+    /* Se só havia um nodo, ao ser retirado a lista fica vazia */
+    if (f->ini == NULL){
+        f->fim = NULL;
+    }
+    
     free(n);
     f->tamanho--;
     return 1;
@@ -58,5 +89,8 @@ int fila_tamanho (struct fila *f) {
 
 /* Retorna 1 se fila vazia, 0 em caso contrario */
 int fila_vazia (struct fila *f) {
-    return (f->tamanho == 0);
+    if(f->tamanho == 0){
+        return 1;
+    }
+    return 0;
 }
